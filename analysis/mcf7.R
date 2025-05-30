6c00be7527dde9fe302be83982db4d863ab7b9b6
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-05-30 11:40:34 -0500

    Removed ncRNA genes from analysis of the MCF-7 data.

--- analysis/mcf7.R
+++ analysis/mcf7.R
@@ -1,10 +1,31 @@
-# GLM-PCA
-# TO DO
-
 # Additive vs. multiplicative.
 LF <- tcrossprod(fit$L,fit$F)
+mu0 <- colSums(counts)/sum(s)
 err_tm <- apply(abs(counts - LF),2,median)
 res <- ldf(fl_nmf,type = "i")
 LF <- with(res,L %*% diag(D) %*% t(F))
 err_nmf <- apply(abs(Y - LF),2,median)
-plot(log10(err_tm),err_nmf,pch = 20)
+i <- which(mu0 > 100 & mu0 < 500)
+plot(log10(err_tm[i]),err_nmf[i],pch = 20)
+
+# NEXT: Look at the results for selected genes.
+
+# Compute the mean expression for each of the four conditions.
+n  <- ncol(counts)
+mu <- data.frame(EtOH = rep(0,n),
+                 RA   = rep(0,n),
+                 TGFb = rep(0,n),
+                 "RA+TGFb" = rep(0,n),
+                 check.names = FALSE)
+for (treatment in levels(samples$label)) {
+  i <- which(samples$label == treatment)
+  mu[[treatment]] <- colSums(counts[i,])/sum(s[i])
+}
+rownames(mu) <- colnames(counts)
+
+i <- 9172
+i <- 24365
+genes[i,1:2]
+mu[i,]
+plot(c(as.matrix(mu[i,])),pch = 20)
+
