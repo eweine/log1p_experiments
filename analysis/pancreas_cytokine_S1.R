e280637055269466e11dcf301e35f43beb3a9ff6
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-06-22 20:26:24 -0500

    wflow_publish("analysis/pancreas_cytokine_S1.Rmd", view = F,     verbose = T)

--- analysis/pancreas_cytokine_S1.R
+++
@@ -1,6 +0,0 @@
-# Compare the factors to the clusters.
-p1 <- structure_plot(L[i,],grouping = clusters[i],gap = 10,n = Inf,
-                     topics = c(1,2,4,5,9,10,11,12,13))
-p2 <- structure_plot(L[i,],grouping = clusters[i],gap = 10,n = Inf,
-                     topics = c(3,6,7,8))
-plot_grid(p1,p2,nrow = 2,ncol = 1)
